db25b1cfa6689c3273a51e7bf85a09bcbaebfaa1
Djibril Thiongane <djibthiongane@gmail.com>
2023-11-26 09:05:34 +0000

    Update mkdjango.py
    remove \n

--- mkdjango.py
+++ mkdjango.py
@@ -34,137 +34,137 @@ def ecrire(pp, n, l):
 # ecrire(f, 50, l)
 
 models = """
-from django.db import models  \n
-from django.contrib.auth.models import User \n
-# Create your models here. \n
- \n
-class Profil(models.Model): \n
-    user=models.OneToOneField(User,on_delete=models.CASCADE) \n
-    avatar=models.ImageField(upload_to="profil/avatar/",null=True,blank=True) \n
-    premium=models.BooleanField(default=False) \n
-    tel1=models.CharField(max_length=12,null=True,blank=True) \n
-    tel2=models.CharField(max_length=12,null=True,blank=True) \n
-    tel3=models.CharField(max_length=12,null=True,blank=True) \n
- \n
-    def __str__(self) -> str: \n
-        return self.user.username \n
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profil(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    avatar=models.ImageField(upload_to="profil/avatar/",null=True,blank=True)
+    premium=models.BooleanField(default=False)
+    tel1=models.CharField(max_length=12,null=True,blank=True)
+    tel2=models.CharField(max_length=12,null=True,blank=True)
+    tel3=models.CharField(max_length=12,null=True,blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
 """
 
 views = f"""
-from django.shortcuts import render \n
-from django.contrib.auth import authenticate ,login,logout \n
-from django.urls import reverse \n
-from django.http import JsonResponse \n
-from {app}.forms import  RegisterForm ,LoginForm \n
-from django.contrib.auth.models import User \n
-from {projet}.settings import BASE_DIR \n
-from django.shortcuts import render ,HttpResponseRedirect \n
-from {app}.models import Profil \n
- \n
- \n
-# Create your views here. \n
- \n
-def index(r): \n
-    ctx={{}} \n
-    return render(r,"{app}/index.html",ctx) \n
- \n
-def isconnect(r): \n
-    if r.user.is_authenticated: \n
-        return True \n
-    return False \n
- \n
-def register(r): \n
-    ctx={{}} \n
-    ctx['error']=[] \n
-    if r.method=="POST": \n
-        form=RegisterForm(r.POST) \n
-        if form.is_valid(): \n
-            username=form.cleaned_data['username'] \n
-            # lastname=form.cleaned_data['lastname'] \n
-            email=form.cleaned_data['email'] \n
-            pwd1=form.cleaned_data['pwd1'] \n
-            pwd2=form.cleaned_data['pwd2'] \n
-            tel1=form.cleaned_data['tel1'] \n
-            tel2=form.cleaned_data['tel2'] \n
-            tel3=form.cleaned_data['tel3'] \n
- \n
-            if pwd1==pwd2: \n
-                user=User.objects.create_user(username=username,first_name="",last_name="",email=email,password=pwd1) \n
-                profil=Profil() \n
-                profil.user=user \n
-                profil.tel1=tel1 \n
-                profil.tel2=tel2 \n
-                profil.tel3=tel3 \n
-                profil.save() \n
-                # creer_dossier_user(username) \n
-                return HttpResponseRedirect(reverse('index')) \n
-            else: \n
-                ctx['error'].append('les mots de pass ne sont pas identiques') \n
-                ctx['form']=form \n
-                return render(r,"{app}/register.html",ctx) \n
- \n
-    else: \n
-        form=RegisterForm() \n
- \n
-    ctx['form']=form \n
-    return render(r,"{app}/register.html",ctx) \n
- \n
-def login_u(r): \n
-    ctx={{ \n
-        'data':'' \n
-    }}\n
-    if r.method=="POST": \n
-        form=LoginForm(r.POST) \n
-        if form.is_valid(): \n
- \n
-            username=form.cleaned_data['username'] \n
-            password=form.cleaned_data['pwd'] \n
- \n
-            user=authenticate(r,username=username,password=password) \n
-            if user is not None: \n
-                login(r,user) \n
-                ctx['user']=user \n
-                return HttpResponseRedirect(reverse('index')) \n
-            else: \n
-                return HttpResponseRedirect(reverse('register')) \n
-    else: \n
-        form=LoginForm() \n
-        ctx['form']=form \n
-    return render(r,"{app}/login.html",ctx) \n
- \n
- \n
- \n
-def logout_user(r): \n
-    logout(r) \n
-    return HttpResponseRedirect(reverse('index')) \n
- \n
-def view_404(request, *args, **kwargs): \n
-    return render(request,'{app}/404.html',{{}}) \n
- \n
-def view_500(request, *args, **kwargs): \n
-    return render(request,'{app}/500.html',{{}}) 
-\n    
+from django.shortcuts import render
+from django.contrib.auth import authenticate ,login,logout
+from django.urls import reverse
+from django.http import JsonResponse
+from {app}.forms import  RegisterForm ,LoginForm
+from django.contrib.auth.models import User
+from {projet}.settings import BASE_DIR
+from django.shortcuts import render ,HttpResponseRedirect
+from {app}.models import Profil
+
+
+# Create your views here.
+
+def index(r):
+    ctx={{}}
+    return render(r,"{app}/index.html",ctx)
+
+def isconnect(r):
+    if r.user.is_authenticated:
+        return True
+    return False
+
+def register(r):
+    ctx={{}}
+    ctx['error']=[]
+    if r.method=="POST":
+        form=RegisterForm(r.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            # lastname=form.cleaned_data['lastname']
+            email=form.cleaned_data['email']
+            pwd1=form.cleaned_data['pwd1']
+            pwd2=form.cleaned_data['pwd2']
+            tel1=form.cleaned_data['tel1']
+            tel2=form.cleaned_data['tel2']
+            tel3=form.cleaned_data['tel3']
+
+            if pwd1==pwd2:
+                user=User.objects.create_user(username=username,first_name="",last_name="",email=email,password=pwd1)
+                profil=Profil()
+                profil.user=user
+                profil.tel1=tel1
+                profil.tel2=tel2
+                profil.tel3=tel3
+                profil.save()
+                # creer_dossier_user(username)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                ctx['error'].append('les mots de pass ne sont pas identiques')
+                ctx['form']=form
+                return render(r,"{app}/register.html",ctx)
+
+    else:
+        form=RegisterForm()
+
+    ctx['form']=form
+    return render(r,"{app}/register.html",ctx)
+
+def login_u(r):
+    ctx={{
+        'data':''
+    }}
+    if r.method=="POST":
+        form=LoginForm(r.POST)
+        if form.is_valid():
+
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['pwd']
+
+            user=authenticate(r,username=username,password=password)
+            if user is not None:
+                login(r,user)
+                ctx['user']=user
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponseRedirect(reverse('register'))
+    else:
+        form=LoginForm()
+        ctx['form']=form
+    return render(r,"{app}/login.html",ctx)
+
+
+
+def logout_user(r):
+    logout(r)
+    return HttpResponseRedirect(reverse('index'))
+
+def view_404(request, *args, **kwargs):
+    return render(request,'{app}/404.html',{{}})
+
+def view_500(request, *args, **kwargs):
+    return render(request,'{app}/500.html',{{}})
+
+
 """
 
 forms = """
-from django import forms\n
+from django import forms
 
-class RegisterForm(forms.Form): \n
-    username=forms.CharField(max_length=150,widget=forms.TextInput(attrs={'placeholder':"Votre nom d'utilisateur",'label':''}))\n
-    # lastname=forms.CharField(max_length=150)\n
-    email=forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'john@gmail.com'}))\n
-    pwd1=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Votre mot de pass'}))\n
-    pwd2=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"Repeter le mot de pass"}))\n
-    tel1=forms.CharField(max_length=12,required=False,widget=forms.TextInput(attrs={'placeholder':"77 000 00 00( optionnel)"}))\n
-    tel2=forms.CharField(max_length=12,required=False,widget=forms.TextInput(attrs={'placeholder':"76 000 00 00( optionnel)"}))\n
-    tel3=forms.CharField(max_length=12,required=False,widget=forms.TextInput(attrs={'placeholder':"70 000 00 00( optionnel)"}))\n
+class RegisterForm(forms.Form):
+    username=forms.CharField(max_length=150,widget=forms.TextInput(attrs={'placeholder':"Votre nom d'utilisateur",'label':''}))
+    # lastname=forms.CharField(max_length=150)
+    email=forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'john@gmail.com'}))
+    pwd1=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Votre mot de pass'}))
+    pwd2=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"Repeter le mot de pass"}))
+    tel1=forms.CharField(max_length=12,required=False,widget=forms.TextInput(attrs={'placeholder':"77 000 00 00( optionnel)"}))
+    tel2=forms.CharField(max_length=12,required=False,widget=forms.TextInput(attrs={'placeholder':"76 000 00 00( optionnel)"}))
+    tel3=forms.CharField(max_length=12,required=False,widget=forms.TextInput(attrs={'placeholder':"70 000 00 00( optionnel)"}))
 
 
 
-class LoginForm(forms.Form): \n
-    username=forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder':"Nom d'utilisateur"}))\n
-    pwd=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"Votre mot de pass"}))\n
-\n
+class LoginForm(forms.Form):
+    username=forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder':"Nom d'utilisateur"}))
+    pwd=forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':"Votre mot de pass"}))
 
 """
 
